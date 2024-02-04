cd2487671dc89caa67be749da645282e6814e749
Ilya Horatio <ilyagoracio@gmail.com>
2024-02-04 21:58:03 +0400

    minor tweaks to privacy policy

--- src/components/AndroidPrivacyPolicy.tsx
+++ src/components/AndroidPrivacyPolicy.tsx
@@ -5,12 +5,13 @@ const AndroidPrivacyPolicy: React.FC = () => {
     return (
         <section>
             <h2>Android Privacy Policy</h2>
+            <p>By <strong>We</strong> it should be understood as <strong>HojaRoja Software</strong>.</p>
             <p>We respect your privacy rights and recognize the importance of protecting any information collected about you. This Privacy Policy defines usage of personal and non-personal information.</p>
             <p>You agree to this privacy policy by installing and using our applications. Please do not install or use our applications if you do not agree with this privacy policy. We reserve the right to modify this Privacy Policy from time to time, so please review it frequently. Your continued use of our applications will signify your acceptance of the changes to this Privacy Policy.</p>
             <h3>Personal Data:</h3>
             <p>We do not collect or use any personal data that may be used to identify or contact you.</p>
             <h3>Non-personal data:</h3>
-            <p>Please see <strong>Advertising Partners Privacy Policies</strong> to see what is shared with Google AdMob for Advertisement purposes.</p>
+            <p>Please see <strong>Advertising Partners Privacy Policies</strong> to see what is shared with Google AdMob for Advertisement purposes. We do not collect any other Non-personal data.</p>
             <h3>Third Party Privacy Policies:</h3>
             <p>HojaRoja Software's Privacy Policy does not apply to other advertisers or websites accessed within HojaRoja Software's applications. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt out of certain options.</p>
             <h3>Advertising Partners Privacy Policies:</h3>
